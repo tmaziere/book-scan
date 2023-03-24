f7c5406915031353aa9a16c69718fd140f3d1b7f
Thomas Maziere <thomas.maziere@incaya.fr>
2023-03-24 17:08:46 +0100

    feat: pdf creation feature, saving in best quality

--- scan.py
+++ scan.py
@@ -3,6 +3,7 @@ from tkinter import *
 from PIL import Image, ImageTk
 import cv2
 import uuid
+from PIL import Image
 
 from utils.analyse_image import is_blurred, list_ports
 
@@ -25,28 +26,41 @@ class MainWindow():
         # Create canvas for image
         self.canvas1 = Canvas(
             self.window, width=self.width, height=self.height)
-        self.canvas1.grid(row=0, column=0, columnspan=2, padx=15, pady=15, sticky=E)
+        self.canvas1.grid(row=0, column=0, columnspan=1,
+                          padx=15, pady=15, sticky=E)
         self.canvas2 = Canvas(
             self.window, width=self.width, height=self.height)
-        self.canvas2.grid(row=0, column=2, columnspan=2, padx=15, pady=15, sticky=W)
-        self.but_preview = Button(self.window, text="Preview (p)", command=self.handle_preview)
+        self.canvas2.grid(row=0, column=1, columnspan=2,
+                          padx=15, pady=15, sticky=W)
+        but_frame = Frame(self.window, width=self.width, height=self.height)
+        but_frame.grid(row=0, column=2, padx=15, pady=15, sticky=S)
+        self.but_preview = Button(
+            but_frame, text="Preview (p)", command=self.handle_preview)
         self.but_preview.grid(row=1, column=0, rowspan=2, ipadx=10, ipady=10)
-        self.but_save = Button(self.window, text="Save (s)", command=self.save_pages)
+        self.but_save = Button(
+            but_frame, text="Save (s)", command=self.save_pages)
         self.but_save.grid(row=1, column=1, rowspan=2, ipadx=10, ipady=10)
+        self.but_create_pdf = Button(
+            but_frame, text="Create PDF", command=self.create_pdf)
+        self.but_create_pdf.grid(
+            row=1, column=2, rowspan=2, ipadx=10, ipady=10)
         self.but_save['state'] = DISABLED
-        label = Label(root, text='Use "preview" to freeze the images : if they are good for you, click "Save" to process the next pages.')
-        label.grid(row=1, column=2, columnspan=2, padx=15, pady=15, sticky=W)
+        # label = Label(
+        #     root, text='Use "preview" to freeze the images : if they are good for you, click "Save" to process the next pages.')
+        # label.grid(row=1, column=2, columnspan=2, padx=15, pady=15, sticky=W)
         # global attributes
         self.previz_mode = False
         self.buffer = {'l': None, 'r': None}
         self.pages = []
         self.page_count = 0
         self.temp_dir_path = 'temp/'
+        self.pdf_books_path = 'books/'
         self.book_uniqid = None
 
         self.start_new_book()
-        label2 = Label(root, text=f'Current book reference: {self.book_uniqid}')        
-        label2.grid(row=2, column=2, columnspan=2, padx=15, pady=15, sticky=W)
+        # label2 = Label(
+        #     root, text=f'Current book reference: {self.book_uniqid}')
+        # label2.grid(row=2, column=2, columnspan=2, padx=15, pady=15, sticky=W)
         self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
         # Update image on canvas
         self.update_image()
@@ -76,13 +90,19 @@ class MainWindow():
 
     def buffer_image(self, index):
         if index in ("l", "r"):
-            img = self.cap[index].read()[1]
+            # @TODO : temporiser le changement de résolution
+            self.cap[index].set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+            self.cap[index].set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+            # @TODO : il faudra certainement effectuer une rotation différente pour les pages gauche et droite
+            img = cv2.rotate(self.cap[index].read()[1], cv2.ROTATE_90_COUNTERCLOCKWISE)
+            self.cap[index].set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+            self.cap[index].set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
             res_is_blurred, blur_val = is_blurred(img, 100)
             print(res_is_blurred, blur_val)
             self.buffer[index] = img
         else:
             raise Exception('Unknown cap index: ', index)
-        
+
     def handle_preview(self):
         self.buffer_image('l')
         self.buffer_image('r')
@@ -131,6 +151,22 @@ class MainWindow():
             print('Error creating book temp directory: ', e)
             pass
 
+    def create_pdf(self):
+        book_files_path = os.path.join(
+            self.temp_dir_path, str(self.book_uniqid))
+        images_count = len([name for name in os.listdir(
+            book_files_path) if os.path.isfile(os.path.join(book_files_path, name))])
+        images = []
+        for img_id in range(1, images_count+1):
+            page_name = 'page_' + str(img_id) + '.jpg'
+            images.append(Image.open(os.path.join(book_files_path, page_name)))
+        pdf_path = os.path.join(
+            self.pdf_books_path, str(self.book_uniqid)+'.pdf')
+        images[0].save(
+            pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:]
+        )
+        print("Nouveau pdf créé")
+
     def on_closing(self):
         print('Closing...')
         self.cap['l'].release()
@@ -140,6 +176,6 @@ class MainWindow():
 
 if __name__ == "__main__":
     root = Tk()
-    # print(list_ports()[1])
-    MainWindow(root, cv2.VideoCapture(0), cv2.VideoCapture(4))
+    #print(list_ports()[1]) # Cette fonction retourne la liste des ports vidéos actifs, à utiliser pour les VideCapture
+    MainWindow(root, cv2.VideoCapture(0), cv2.VideoCapture(5))
     root.mainloop()
